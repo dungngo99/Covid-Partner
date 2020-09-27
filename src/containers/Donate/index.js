28092945ba3a1740930a044d26a1f2fafa8942bb
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-09-27 02:46:39 -0500

    using stuff from firebase

--- src/containers/Donate/index.js
+++ src/containers/Donate/index.js
@@ -1,156 +1,112 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {ThemeConsumer} from 'styled-components';
 import {
   Container,
   Nav,
   NavItem,
-  FormCheckbox,
   Row,
   Col,
   Card,
   CardTitle,
   CardBody,
   CardImg,
-  CardFooter,
   Button
 } from 'shards-react';
+import firebase from 'firebase/app';
 
 const _chunk = require('lodash.chunk');
 
+const ItemRequest = ({item}) => {
+  return (
+    <Card>
+      <CardBody>
+        <CardTitle>
+          {item.user.firstName} {item.user.lastName}
+          <Button outline>
+            Contact
+          </Button>
+        </CardTitle>
+        {item.title}
+      </CardBody>
+      {item.image && <CardImg bottom src={item.image} style={{'maxHeight': '20vh', 'objectFit': 'cover'}} />}
+    </Card>
+  );
+};
+
 const Donate = () => {
 
-  const filterNames = {
+  const [loading, setLoading] = useState(true);
+  const [requests, setRequests] = useState([]);
+  const [active, setActive] = useState(true);
 
-  };
+  useEffect(() => {
+    const unsubscribe  = firebase.firestore().collection('Donate_post_id').onSnapshot((snapshot) => {
+      const req = [];
+      snapshot.forEach((doc) => {
+        let _doc = {...doc.data()}
+        doc.data().userRef.get().then((res) => {
+          _doc.user = res.data()
+          req.push(_doc);
+        })
+        console.log(_doc);
+      });
+      setLoading(false);
+      setRequests(req);
+    });
 
-  const ItemRequest = ({item}) => {
-    return (
-      <Card>
-        <CardBody>
-          <CardTitle>
-            {item.user.firstName} {item.user.lastName[0]}.
-            <Button outline>
-              Contact
-            </Button>
-          </CardTitle>
-          {item.title}
-        </CardBody>
-        {item.image && <CardImg bottom src={item.image} style={{'maxHeight': '20vh', 'objectFit': 'cover'}} />}
-        <CardFooter>
-
-        </CardFooter>
-      </Card>
-    );
-  };
-
-  const requests = [
-    {
-      user: {
-        firstName: "Bob",
-        lastName: "Ross"
-      },
-      title: "Paint",
-      image: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Multicolored_tempera_paints.jpg"
-    }, 
-    {
-      user: {
-        firstName: "Gordon",
-        lastName: "Ramsay"
-      },
-      title: "Lamb Sauce",
-      image: "https://assets.marthastewart.com/styles/wmax-300/d21/a99872_0303_lambgravy/a99872_0303_lambgravy_vert.jpg"
-    }, 
-    {
-      user: {
-        firstName: "Sam",
-        lastName: "Smith"
-      },
-      title: "Masks x25",
-      image: "https://cdn.discordapp.com/attachments/758010039729389642/759639137040072705/61j3WQQbHjL.png"
-    }, 
-    {
-      user: {
-        firstName: "David",
-        lastName: "Ross"
-      },
-      title: "DUDE WHO'S FRIENDZONED ALWAYS",
-      image: "https://www.humaverse.com/wp-content/uploads/2019/10/1229px-David_Schwimmer_Jul_2005_London_England_Cropblur1.jpg"
-    },
-     { 
-      user: {
-        firstName: 'Scrooge',
-        lastName: 'McDuck'
-      }, 
-      title: "RICHEST DUCK IN THE ENTIRE CARTOON MUlTIVERSE",
-      image: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Multicolored_tempera_paints.jpg"
-    },
-    {
-      user: {
-        firstName: 'Miley',
-        lastName: 'Cyrus'
-      },
-      title: "Side by Side Comparison",
-      image: "https://i.imgur.com/bHPTygz.jpg"
-    },
-    { 
-      user: {
-        firstName: 'Billy',
-        lastName: 'Ray Cyrus'
-      }, 
-      title: "Ex-Country Pop star",
-      image: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Multicolored_tempera_paints.jpg"
-    },
-    { 
-      user: {
-        firstName: 'Dolly',
-        lastName: 'Parton'
-      }, 
-      title: "Middle aged celebrity ",
-      image: "https://celebmafia.com/wp-content/uploads/2019/02/dolly-parton-2019-grammy-awards-2.jpg"
-    },
-    {
-      user: {
-        firstName: 'Leoluch',
-        lastName: 'the_Rebellion'
-      }, 
-      title: "Smartass Dictator",
-      image: "http://3.bp.blogspot.com/-yVe-wtSYqeE/Vcs131AlNBI/AAAAAAAAAYo/NJ__DYcuVzI/s1600/%255BCoalgirls%255D_Code_Geass_23_%25281280x720_Blu-ray_FLAC%2529_%255B51DD6992%255D.mkv_snapshot_21.43_%255B2015.06.29_17.20.43%255D.png"
+    return () => {
+      unsubscribe();
     }
-];
+  }, []);
 
   return (
     <Container fluid>
-      <Nav fill>
-        <NavItem>
-          Inactive
-        </NavItem>
-        <NavItem>
-          Active
-        </NavItem>
-      </Nav>
-      <Nav fill>
-        <NavItem>
-          <FormCheckbox inline></FormCheckbox>
-        </NavItem>
-        <NavItem>
-          <FormCheckbox inline></FormCheckbox>
-        </NavItem>
-        <NavItem>
-          <FormCheckbox inline></FormCheckbox>
-        </NavItem>
-      </Nav>
+      <ThemeConsumer>
+        {
+          theme => (
+            <Nav fill>
+              <NavItem
+                onClick={() => setActive(false)}
+                style={{
+                  'fontSize': '30px',
+                  'fontWeight': !active ? 'bold' : '',
+                  'color': !active ? theme.darkblue : ''
+                }}
+              >
+                Inactive
+              </NavItem>
+              <NavItem
+                onClick={() => setActive(true)}
+                style={{
+                  'fontSize': '30px',
+                  'fontWeight': active ? 'bold' : '',
+                  'color': active ? theme.darkblue : ''
+                }}
+              >
+                Active
+              </NavItem>
+            </Nav>
+          )
+        }
+      </ThemeConsumer>
       <Container>
         {
-          _chunk(requests, 5).map((row) => {
+          !loading &&
+          _chunk(requests.filter((val) => val.active === active), 3).map((row, i) => {
             return (
-              <Row>
+              <Row key={i}>
                 {
-                  row.map((col) => (
-                    <Col><ItemRequest item={col} /></Col>
+                  row.map((col, j) => (
+                    <Col key={j}><ItemRequest item={col} /></Col>
                   ))
                 }
               </Row>
             )
           })
+        }
+        {
+          loading &&
+          <div>Loading...</div>
         }
       </Container>
     </Container>
